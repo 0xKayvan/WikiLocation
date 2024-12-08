cfd7788bf18daf9ea93f4eeba94dc8ea0658d81b
Kayvan Nouri <0xkayvan@gmail.com>
2024-12-08 20:15:03 +0100

    Added LocationsView UI elements

--- WikiLocation/Views/LocationsView.swift
+++ WikiLocation/Views/LocationsView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct LocationsView: View {
-    private let tempLocations = Location.mocks(count: 5)
+    private let locationManager: LocationManager = .shared
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(tempLocations) { location in
-                    LocationCellView(location: location)
+                Section(header: Text("Remote")) {
+                    if locationManager.remoteLocations.isEmpty {
+                        HStack {
+                            Image(systemName: "network.slash")
+                            Text("No remote locations available")
+                                .font(.subheadline)
+                        }
+                        .foregroundColor(.secondary)
+                    } else {
+                        ForEach(locationManager.remoteLocations) { location in
+                            LocationCellView(location: location)
+                        }
+                    }
+                }
+                
+                Section(header: Text("Local")) {
+                    if locationManager.localLocations.isEmpty {
+                        NavigationLink {
+                            // Navigate to AddLocationView
+                        } label: {
+                            Image(systemName: "plus")
+                            Text("Add Local Location")
+                        }
+                        .foregroundColor(.accentColor)
+                    } else {
+                        ForEach(locationManager.localLocations) { location in
+                            LocationCellView(location: location)
+                        }
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                        .disabled(locationManager.localLocations.isEmpty)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        // Navigate to AddLocationView
+                    } label: {
+                        Image(systemName: "plus")
+                    }
                 }
             }
             .navigationTitle("Locations")
