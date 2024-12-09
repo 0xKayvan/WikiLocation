383fdf8b31446d993f737513f0c67c799ef13dd5
Kayvan Nouri <0xkayvan@gmail.com>
2024-12-10 00:01:07 +0100

    Completed Location Model Unit Tests

--- WikiLocationTests/LocationModelTests.swift
+++ WikiLocationTests/LocationModelTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class LocationModelTests: XCTestCase {
 
-    func testLocationInit() {
+    func testLocationInit() throws {
         let location = Location(name: "Test", latitude: 1.0, longitude: 2.0)
         XCTAssertNotNil(location.id)
         XCTAssertEqual(location.name, "Test")
@@ -18,7 +18,7 @@ final class LocationModelTests: XCTestCase {
         XCTAssertEqual(location.longitude, 2.0)
     }
     
-    func testLocationInitNoName() {
+    func testLocationInitNoName() throws {
         let location = Location(latitude: 1.0, longitude: 2.0)
         XCTAssertNotNil(location.id)
         XCTAssertNil(location.name)
@@ -26,8 +26,23 @@ final class LocationModelTests: XCTestCase {
         XCTAssertEqual(location.longitude, 2.0)
     }
     
+    func testLocationCoordinateRangeValidity() throws {
+        XCTAssertTrue(Location.areCoordinatesValid(latitude: 0.0, longitude: 0.0), "Valid coordinates should be indicated as valid")
+        XCTAssertTrue(Location.areCoordinatesValid(latitude: 90.0, longitude: 180.0), "Valid coordinates should be indicated as valid")
+        XCTAssertTrue(Location.areCoordinatesValid(latitude: 90.0, longitude: -180.0), "Valid coordinates should be indicated as valid")
+        XCTAssertTrue(Location.areCoordinatesValid(latitude: -90.0, longitude: 180.0), "Valid coordinates should be indicated as valid")
+        XCTAssertTrue(Location.areCoordinatesValid(latitude: -90.0, longitude: -180.0), "Valid coordinates should be indicated as valid")
+        
+        
+        XCTAssertFalse(Location.areCoordinatesValid(latitude: 91.0, longitude: 0.0), "Invalid coordinates should be indicated as invalid")
+        XCTAssertFalse(Location.areCoordinatesValid(latitude: -91.0, longitude: 0.0), "Invalid coordinates should be indicated as invalid")
+        
+        XCTAssertFalse(Location.areCoordinatesValid(latitude: 0.0, longitude: 181.0), "Invalid coordinates should be indicated as invalid")
+        XCTAssertFalse(Location.areCoordinatesValid(latitude: 0.0, longitude: -181.0), "Invalid coordinates should be indicated as invalid")
+    }
+    
     // MARK: - Mockable Tests
-    func testMockability() {
+    func testMockability() throws {
         let location = Location.mock()
         // Ignore the Xcode warning which says "'is' test is always true"
         // Swift compiler is type-checking the protocol conformity,
@@ -36,7 +51,7 @@ final class LocationModelTests: XCTestCase {
         XCTAssertTrue(location is (any Mockable), "Location should be mockable")
     }
     
-    func testStub() {
+    func testStub() throws {
         let location = Location.stub()
         XCTAssertNotNil(location.id)
         XCTAssertEqual(location.name, "Amsterdam", "Default stub name should be Amsterdam")
@@ -44,7 +59,7 @@ final class LocationModelTests: XCTestCase {
         XCTAssertEqual(location.longitude, 4.9041, "Default stub longitude should be 4.9041")
     }
     
-    func testMock() {
+    func testMock() throws {
         let location = Location.mock()
         XCTAssertNotNil(location.id)
         XCTAssertNotNil(location.name)
@@ -52,7 +67,7 @@ final class LocationModelTests: XCTestCase {
         XCTAssertNotNil(location.longitude)
     }
     
-    func testMocks() {
+    func testMocks() throws {
         let locations = Location.mocks(count: 3)
         XCTAssertEqual(locations.count, 3, "Should return 3 locations")
         locations.forEach { location in
@@ -62,6 +77,33 @@ final class LocationModelTests: XCTestCase {
             XCTAssertNotNil(location.longitude)
         }
     }
+    
+    // MARK: - Codable Tests
+    func testCodingKeys() throws {
+        let idKey = "id"
+        let nameKey = "name"
+        let latitudeKey = "lat"
+        let longitudeKey = "long"
+        XCTAssertEqual(Location.CodingKeys.id.rawValue, idKey, "Location id key should be \(idKey)")
+        XCTAssertEqual(Location.CodingKeys.name.rawValue, nameKey, "Location name key should be \(nameKey)")
+        XCTAssertEqual(Location.CodingKeys.latitude.rawValue, latitudeKey, "Location latitude key should be \(latitudeKey)")
+        XCTAssertEqual(Location.CodingKeys.longitude.rawValue, longitudeKey, "Location longitude key should be \(longitudeKey)")
+    }
+    
+    func testCodable() throws {
+        let location = Location(name: "Test", latitude: 1.0, longitude: 2.0)
+        let encoder = JSONEncoder()
+        let decoder = JSONDecoder()
+        let data = try? encoder.encode(location)
+        XCTAssertNotNil(data, "Encoded data should not be nil")
+        let decodedLocation = try? decoder.decode(Location.self, from: data!)
+        XCTAssertNotNil(decodedLocation, "Decoded location should not be nil")
+        XCTAssertEqual(location.id, decodedLocation!.id, "Decoded location id should match the original location id")
+        XCTAssertEqual(location.name, decodedLocation!.name, "Decoded location name should match the original location name")
+        XCTAssertEqual(location.latitude, decodedLocation!.latitude, "Decoded location latitude should match the original location latitude")
+        XCTAssertEqual(location.longitude, decodedLocation!.longitude, "Decoded location longitude should match the original location longitude")
+    }
+        
 }
 
 
